1d9a7e9e570dd6d333058c9e7d5d65d676cf3fb6
rauldedie <105632651+rauldedie@users.noreply.github.com>
2022-10-31 17:09:36 +0100

    Add files via upload

--- javascript/ejercicio2.js
+++ javascript/ejercicio2.js
@@ -1,12 +1,14 @@
 function cambiodivisa() {
 
-    let a = 0 , b = 0;
-    a = parseFloat(document.getElementById("euro").value);
-    if (a=="NaN") {
+    let a = 0 , b = 0, dolar;
+    euro = document.getElementById("euro").value;
+
+    if (euro.length==0) {
         b = "Debes introducir un numero";
     }
     else{
 
+        a = parseFloat(euro);
         b = a * 0.99;
         
     }
