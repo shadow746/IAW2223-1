84558f5f4c7ec5ac6c4dfae7db27f15849b983dd
rauldedie <rauldedie@gmail.com>
2022-11-07 20:42:50 +0100

    ejercicio1 corregido

--- javascript/ejercicio1.js
+++ javascript/ejercicio1.js
@@ -1,12 +1,12 @@
-function ocultarboton1(id) {
-
-    document.getElementById("rojo").style.display = "none";
-}
-function ocultarboton2(id) {
-
-    document.getElementById("azul").style.display = "none";
-}
-function ocultarboton3(id) {
-
-    document.getElementById("amarillo").style.display = "none";
+function ocultarcirculo(id) 
+{
+    
+    if (document.getElementById(id).style.visibility == "hidden")
+    {
+        document.getElementById(id).style.visibility = "visible";
+    }
+    else{
+        document.getElementById(id).style.visibility = "hidden";
+    }
+   
 }
