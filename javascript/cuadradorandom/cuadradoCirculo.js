97c6531c9240671d8dc21de1187089dd1d8f20ee
rauldedie <rauldedie@gmail.com>
2022-11-15 00:15:46 +0100

    ok

--- javascript/cuadradorandom/cuadradoCirculo.js
+++ javascript/cuadradorandom/cuadradoCirculo.js
@@ -1,13 +1,13 @@
 function empiezajuego(espera){
     document.getElementById("resultado").innerHTML="";
     let i;
-    let cuadrado,circulo,muestra,respuesta;
+    let figura,respuesta;
     
     for (i=0;1<espera;i++); 
 
     muestra = parseInt(Math.random()*10)%2;
 
-    respuesta = muestrafigura(muestra);
+    respuesta = muestrafigura(figura);
     espera = parseInt(Math.random()*20);
     document.getElementById("respuesta").innerHTML = "Has tardado "+respuesta+" segundos."
 
@@ -18,17 +18,22 @@ function empiezajuego(espera){
 function muestrafigura(muestra)
 {
     let figura;
-    let ancho,alto,color;
+    let ancho,alto,color,tiempo;
 
     ancho = parseInt(Math.random()*(350-100)+100);
     alto = parseInt(Math.random()*(350-100)+100);
     color =parseInt(Math.random()*255);
 
+    let tiempo = 
+
     if (muestra == 0) {
-        figura = document.createElement("div");
-        document.appendChild(figura);
-        figura.style.backgroundColor = color;
-        figura.style.width = ancho;
-        figura.style.height = alto;
+        figura = document.createElement("div");        
+            figura.style.backgroundColor = color;
+            figura.style.width = ancho;
+            figura.style.height = alto;
+            figura.style.borderRadius = 100;
+            figura.setAttribute("onclick","tiempo()")
+        document.getElementById("resultado").append(figura);
+
     }
 }
