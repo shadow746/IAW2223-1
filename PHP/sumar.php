3d7ec01bcffc38c4a5e3c987ca702e1789ee240c
rauldedie <rauldedie@gmail.com>
2022-12-09 12:21:33 +0100

    ok

---
+++ PHP/sumar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Numer9cos</title>
+</head>
+<body>
+<?php
+    $a=5;
+    $b=6;
+    $cadena = "Esto es mi variable de cadena";
+    
+    echo "<p>suma x+y=" .   $a+$b . "</p><br>";
+    echo "<p>resta x-y=" .   $a-$b . "</p><br>";
+    echo "<p>multiplicar x*y=" .   $a*$b . "</p><br>";
+    echo "<p>dividir x/y=" .   $a/$b . "</p><br>";
+?>
+</body>
+</html>
+
