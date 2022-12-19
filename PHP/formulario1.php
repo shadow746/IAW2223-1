35c04f88f2ebde72e671c609a8d6dd2c4f2672e7
rauldedie <rauldedie@gmail.com>
2022-12-20 00:23:14 +0100

    ok

--- PHP/formulario1.php
+++ PHP/formulario1.php
@@ -7,11 +7,11 @@
     <title>Formulario Usuario</title>
 </head>
 <body>
- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+ <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <label for="usuario">Dime nombre de usuario</label>
-    <input type="text" name="usuario" method="post"><br><br>
+    <input type="text" name="usuario"><br><br>
     <label for="passwd">Dime la contraseña</label>
-    <input type="password" name="passwd" method="post"><br><br>
+    <input type="password" name="passwd"><br><br>
     <input type="submit" name="submit"><br><br>
 </form>  
 <?php
