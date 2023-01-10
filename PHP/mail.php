418b41e1e8fcd2d922a0d7ac27a2032fb32188a6
rauldedie <rauldedie@gmail.com>
2023-01-10 21:06:28 +0100

    ok

--- PHP/mail.php
+++ PHP/mail.php
@@ -4,38 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enviar un mail</title>
+    <title>Document</title>
 </head>
 <body>
 <h1>Vamos a enviar un mail</h1>
 
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
     <label for="destinatario">Dime el destinatario: </label>
     <input type="email" name ="destinatario" placeholder="Email destinatario"> <br><br>
+
     <label for="asunto">Dime el asunto: </label>
     <input type="text" name ="asunto" placeholder="Asunto"> <br><br>
+
     <label for="mensaje">Escribe el texto del mensaje: </label> <br>
     <textarea name="mensaje" cols="30" rows="10"></textarea> <br><br>
+
     <input type="submit" name="submit">
-</form>  
+
+</form>
 <?php
-if(isset($_POST["submit"]))
+if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-        
-        $asunto = htmlspecialchars($_POST["asunto"]);
-        $mensaje = htmlspecialchars($_POST["mensaje"]);
-        $cabecera = "From: Papichulo<cumples@example.com>" . "\r\n";
-        $para = test_input($_POST["destinatario"]);
-
-        if (!filter_var($para, FILTER_VALIDATE_EMAIL)) 
-        {
-            echo "Formato del email invalido";
-
-        }else
-        {           
-            mail($para,$asunto,$mensaje,$cabecera);
-        }
-
+    $asunto = $_POST["asunto"];
+    $mensaje = $_POST["mensaje"];
+    $cabecera = "From: Recordatorio <cumples@example.com>" . "\r\n";
+    $correo = test_input($_POST["email"]);
+    if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) 
+    {
+        $emailErr = "Invalid email format";
+    }else
+    {
+        mail($correo,$asunto,$mensaje,$cabecera);
+    }
 }
 ?>
 </body>
