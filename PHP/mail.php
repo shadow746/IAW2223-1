b6fb062210fd6920cdbad4b45588f98d31728cfb
rauldedie <rauldedie@gmail.com>
2023-01-10 20:45:37 +0100

    ok

--- PHP/mail.php
+++ PHP/mail.php
@@ -11,17 +11,31 @@
 
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <label for="destinatario">Dime el destinatario: </label>
-    <input type="text" name ="destinatario" placeholder="Email destinatario"> <br><br>
-    <label for="texto">Escribe el texto del mensaje: </label> <br>
-    <textarea name="texto" cols="30" rows="10"></textarea> <br><br>
+    <input type="email" name ="destinatario" placeholder="Email destinatario"> <br><br>
+    <label for="asunto">Dime el asunto: </label>
+    <input type="text" name ="asunto" placeholder="Asunto"> <br><br>
+    <label for="mensaje">Escribe el texto del mensaje: </label> <br>
+    <textarea name="mensaje" cols="30" rows="10"></textarea> <br><br>
     <input type="submit" name="submit">
 </form>  
 <?php
 if(isset($_POST["submit"]))
 {
-    $destinatario = $_POST["destinatario"];
-    $mensaje = $_POST["texto"];
-    mail($destinatario,"Importante","papichulo",$mensaje);
+        
+        $asunto = htmlspecialchars($_POST["asunto"]);
+        $mensaje = htmlspecialchars($_POST["mensaje"]);
+        $cabecera = "From: Papichulo<cumples@example.com>" . "\r\n";
+        $para = test_input($_POST["destinatario"]);
+
+        if (!filter_var($para, FILTER_VALIDATE_EMAIL)) 
+        {
+            echo "Formato del email invalido";
+
+        }else
+        {           
+            mail($para,$asunto,$mensaje,$cabecera);
+        }
+
 }
 ?>
 </body>
