20483dd1d9c7ce1e9a7cf48ae2f8206e47eb3cca
rauldedie <rauldedie@gmail.com>
2023-01-13 12:49:27 +0100

    ok

---
+++ PHP/conectar.php
@@ -0,0 +1,27 @@
+<?php
+$servidor="sdb-53.hosting.stackcp.net";
+$usuario="rauldedie";
+$password="lince123";
+$bd="bdpruebas-353030355619";
+
+$enlace = mysqli_connect($servidor,$usuario,$password,$bd);
+
+if(!$enlace)
+{
+    echo "Conexion fallida: ".mysqli_connect_error();
+}else
+{
+    echo "Conexion con exito.";
+    $squery = "SELECT * FROM usuarios where username='rauldedie'";
+    $resultado = mysqli_query($enlace.$query);
+    if($resultado)
+    {
+        $fila = mysqli_fecth_array ($resultado);
+        print_r($fila);
+        print ($fila);
+    }
+    mysqli_close($enlace);
+}
+
+
+?>
